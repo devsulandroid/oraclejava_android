293f2679791c69f34859ba57e20a10f9e47e01b7
설의석 <devsul@naver.com>
2016-04-16 17:13:02 +0900

    메뉴 내용 추가

--- A03Menu/app/src/main/java/com/devsul/a03menu/MainActivity.java
+++ A03Menu/app/src/main/java/com/devsul/a03menu/MainActivity.java
@@ -2,6 +2,7 @@ package com.devsul.a03menu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // 옵션 메뉴 만들기
+    // 1. xml 생성 menu_main.xml
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
     }
 }
