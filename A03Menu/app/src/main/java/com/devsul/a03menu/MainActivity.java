ecfdacd6ff1de520632aaebee8b0e38cef66daf7
설의석 <devsul@naver.com>
2016-04-16 17:23:37 +0900

    작업 내용 추가

--- A03Menu/app/src/main/java/com/devsul/a03menu/MainActivity.java
+++ A03Menu/app/src/main/java/com/devsul/a03menu/MainActivity.java
@@ -3,6 +3,8 @@ package com.devsul.a03menu;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +14,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    // 옵션 메뉴 만들기
+    // 옵션 메뉴 만들기 (옵션 메뉴가 생성 될때)
     // 1. xml 생성 menu_main.xml
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
+        //return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    // 옵션 메뉴를 선택 했을때
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //return super.onOptionsItemSelected(item);
+        if(item.getItemId() == R.id.menu01){
+            Toast.makeText(this, "SELECTED", Toast.LENGTH_LONG).show();
+        }
+
+        return true;
     }
 }
