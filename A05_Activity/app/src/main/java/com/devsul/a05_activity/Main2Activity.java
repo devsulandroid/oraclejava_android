0126260a6162136d8ea78324d284746e63fb999f
설의석 <devsul@naver.com>
2016-04-17 13:40:36 +0900

    액티비티

--- A05_Activity/app/src/main/java/com/devsul/a05_activity/Main2Activity.java
+++ A05_Activity/app/src/main/java/com/devsul/a05_activity/Main2Activity.java
@@ -1,7 +1,9 @@
 package com.devsul.a05_activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Intent intent = getIntent();
+
+        String getIdData = intent.getStringExtra("myId");
+
+        Toast.makeText(Main2Activity.this, "get id data : " + getIdData, Toast.LENGTH_LONG).show();
+
     }
 }
