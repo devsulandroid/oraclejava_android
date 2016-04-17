77ef47ba5129998ef39b08841b3c03b336dbe5ce
설의석 <devsul@naver.com>
2016-04-17 13:56:41 +0900

    액티비티

--- A05_Activity/app/src/main/java/com/devsul/a05_activity/Main2Activity.java
+++ A05_Activity/app/src/main/java/com/devsul/a05_activity/Main2Activity.java
@@ -3,6 +3,8 @@ package com.devsul.a05_activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
@@ -18,5 +20,19 @@ public class Main2Activity extends AppCompatActivity {
 
         Toast.makeText(Main2Activity.this, "get id data : " + getIdData, Toast.LENGTH_LONG).show();
 
+
+        Button btn02 = (Button)findViewById(R.id.btn02);
+
+        btn02.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent01 = new Intent();
+                intent01.putExtra("myResult","Main 2 Result");
+                setResult(RESULT_OK, intent01);
+
+                // 자기 자신 종료
+                finish();
+            }
+        });
     }
 }
