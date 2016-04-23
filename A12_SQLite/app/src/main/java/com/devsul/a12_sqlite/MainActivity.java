0f40c1cd86e29803e0eec570e7b8c3176613b1ee
설의석 <devsul@naver.com>
2016-04-23 15:34:58 +0900

    SQLite

--- A12_SQLite/app/src/main/java/com/devsul/a12_sqlite/MainActivity.java
+++ A12_SQLite/app/src/main/java/com/devsul/a12_sqlite/MainActivity.java
@@ -2,6 +2,9 @@ package com.devsul.a12_sqlite;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +13,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView textView = (TextView)findViewById(R.id.textView);
         // 디비가 생성 되어 있으면 디비 생성을 하지 않는다.
         // TESTSQLiteOpenHelper helper = new TESTSQLiteOpenHelper(this, "people", null, 1);
+
+        TESTSQLiteHandler dbHander = new TESTSQLiteHandler(this);
+        dbHander.insert("DEVSUL", 20, "서울");
+        dbHander.insert("설의석", 30, "경기");
+
+        dbHander.update("DEVSUL", 30);
+
+        String res = dbHander.selectAll();
+
+        textView.setText(res);
+
+
+
     }
 }
