82b00659704a728ac261c8d0ee7b6a6b71fd7344
설의석 <devsul@naver.com>
2016-04-23 14:37:56 +0900

    SQLite

---
+++ A12_SQLite/app/src/main/java/com/devsul/a12_sqlite/TESTSQLiteOpenHrlper.java
@@ -0,0 +1,34 @@
+package com.devsul.a12_sqlite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by c on 2016-04-23.
+ * SQLiteOpenHelper 사용하기 위한 커스텀 SQLiteOpenHelper 생성
+ */
+public class TESTSQLiteOpenHrlper extends SQLiteOpenHelper {
+
+    public TESTSQLiteOpenHrlper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    //디비가 처음 한번만 실행
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE USER (id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                     "nabe TEXT, age, INTEGER, address TEXT)";
+
+        db.execSQL(sql);
+    }
+
+    //디비 버전이 높을때 한번 실행됨
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String sql = "DROP TABLE IF EXISTS USER";
+        db.execSQL(sql);
+
+        onCreate(db);
+    }
+}
