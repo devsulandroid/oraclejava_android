0ae6f0ea2ff0de4196e31fbc85747f4f60f750cf
설의석 <devsul@naver.com>
2016-04-23 11:00:48 +0900

    thread

--- A09_Thread/app/src/main/java/com/devsul/a09_thread/MainActivity.java
+++ A09_Thread/app/src/main/java/com/devsul/a09_thread/MainActivity.java
@@ -3,6 +3,9 @@ package com.devsul.a09_thread;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final TextView textView = (TextView)findViewById(R.id.textView) ;
+
+        // Thread
         Thread th = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -23,9 +29,12 @@ public class MainActivity extends AppCompatActivity {
                         throw new RuntimeException("에러");
                     }
                     Log.d("thread","count : " + i);
+                    textView.setText("Count : " + i);
                 }
             }
         });
+
+        // Thread 실행
         th.start();
 
 
