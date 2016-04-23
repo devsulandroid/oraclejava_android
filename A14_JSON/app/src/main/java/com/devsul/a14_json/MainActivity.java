ce69edc81d0dd28606d83a722f48f6acf66f887f
설의석 <devsul@naver.com>
2016-04-23 18:23:49 +0900

    JSON

--- A14_JSON/app/src/main/java/com/devsul/a14_json/MainActivity.java
+++ A14_JSON/app/src/main/java/com/devsul/a14_json/MainActivity.java
@@ -1,13 +1,41 @@
 package com.devsul.a14_json;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    String str = "[{'name':'user_A', 'age':10, 'tel':'010-0001-0101'},"+
+                 "{'name':'user_B', 'age':20, 'tel':'020-0002-0202'},"+
+                 "{'name':'user_B', 'age':30, 'tel':'030-0003-0303'}]";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = (TextView)findViewById(R.id.textView);
+        String res = "";
+        try {
+            //JSONArray 읽기
+            JSONArray array = new JSONArray(str);
+
+            for(int i=0; i < array.length() ; i++){
+                JSONObject object = array.getJSONObject(i);
+                String name = object.getString("name");
+                Integer age = object.getInt("age");
+                String tel = object.getString("tel");
+
+                res += "name : " + name + ", age : " + age + ", tel : " + tel + "\n";
+
+                textView.setText(res);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
