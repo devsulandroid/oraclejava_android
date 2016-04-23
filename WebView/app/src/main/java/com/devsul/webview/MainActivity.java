ac0fdf9727a9c50dd0e0154f21660925aadaec4b
설의석 <devsul@naver.com>
2016-04-23 09:58:29 +0900

    WebView

--- WebView/app/src/main/java/com/devsul/webview/MainActivity.java
+++ WebView/app/src/main/java/com/devsul/webview/MainActivity.java
@@ -2,9 +2,12 @@ package com.devsul.webview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,13 +16,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WebView webView = (WebView)findViewById(R.id.webView);
+        final WebView webView = (WebView)findViewById(R.id.webView);
 
         // 웹뷰 셋팅 얻어와서 자바스크립스 사용 가능하게 설정
         WebSettings ws = webView.getSettings();
-        ws.setJavaScriptEnabled(true);;
+        ws.setJavaScriptEnabled(true);
 
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("http://www.naver.com");
+
+        //버튼 클릭 이벤트
+        Button btnOk = (Button)findViewById(R.id.btnOk);
+
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //버튼 클릭 이벤트
+                EditText editText = (EditText)findViewById(R.id.editText);
+                String getUrl = editText.getText().toString();
+                webView.loadUrl(getUrl);
+            }
+        });
     }
 }
