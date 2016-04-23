cf5a551da3d1af8ad53653fbfd09f92867943604
설의석 <devsul@naver.com>
2016-04-23 11:09:31 +0900

    thread

--- A09_Thread/app/src/main/java/com/devsul/a09_thread/MainActivity.java
+++ A09_Thread/app/src/main/java/com/devsul/a09_thread/MainActivity.java
@@ -1,5 +1,7 @@
 package com.devsul.a09_thread;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,6 +10,15 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int Log_COUNT = 1;
+
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +40,16 @@ public class MainActivity extends AppCompatActivity {
                         throw new RuntimeException("에러");
                     }
                     Log.d("thread","count : " + i);
-                    textView.setText("Count : " + i);
+                    //Thread는 UI에 직업 접근 할 수 없다
+                    //Handler를 통해 처리를 진행한다
+                    //-> 불가능 textView.setText("Count : " + i);
+
+
+                    //Handler 이용
+                    Message msg = new Message();
+                    msg.what = Log_COUNT;
+                    msg.arg1 = i;
+                    handler.sendMessage(msg);
                 }
             }
         });
