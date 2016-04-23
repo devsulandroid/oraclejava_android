aee9b5d59324f440a3ea171c056d5b521a15583f
설의석 <devsul@naver.com>
2016-04-23 17:13:42 +0900

    기상청 xml

--- A13_XML_Parser1/app/src/main/java/com/devsul/a13_xml_parser1/MainActivity.java
+++ A13_XML_Parser1/app/src/main/java/com/devsul/a13_xml_parser1/MainActivity.java
@@ -3,8 +3,10 @@ package com.devsul.a13_xml_parser1;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
@@ -18,6 +20,7 @@ import javax.xml.parsers.ParserConfigurationException;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
     // Thread를 통해 인터넷에서 데이터를 가져오도록 해야한다다
     // Thread + Handler
     // 시작할때 업데이트할때 종료시 사용되는 파라미터 정의가 중요하다
@@ -53,14 +56,31 @@ public class MainActivity extends AppCompatActivity {
 
             String res = "";
             NodeList nodeList = document.getElementsByTagName("data");
+            for(int i=0; i < nodeList.getLength(); i++){
+                Element element = (Element) nodeList.item(i);
+                String day = getNodeValue(element, "day");
+                String hour = getNodeValue(element, "hour");
+                String temp = getNodeValue(element, "temp");
+                String wfKor = getNodeValue(element, "wfKor");
 
+                res += "day : " + day + ", hour : " + hour + ", temp : " + temp + ", wfKor : " + wfKor + "\n";
+            }
+            textView.setText(res);
+        }
+
+        // return Element Value
+        protected String getNodeValue(Element element, String nodeName){
+            NodeList dayNodeList =  element.getElementsByTagName(nodeName);
+            Element dayElement = (Element) dayNodeList.item(0);
+            NodeList textNodeList = dayElement.getChildNodes();
+            return textNodeList.item(0).getNodeValue();
         }
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        textView = (TextView)findViewById(R.id.textView);
         MyDomparser myDomparser = new MyDomparser();
         myDomparser.execute("http://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=1174061000");
 
