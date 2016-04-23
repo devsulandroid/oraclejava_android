b4fa5ae46c4bbc557232e6aac0d08545c9de635d
설의석 <devsul@naver.com>
2016-04-23 12:21:22 +0900

    Thread 2

--- A10_AsycTask/app/src/main/java/com/devsul/a10_asyctask/MainActivity.java
+++ A10_AsycTask/app/src/main/java/com/devsul/a10_asyctask/MainActivity.java
@@ -1,13 +1,39 @@
 package com.devsul.a10_asyctask;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Thread + Handler
+    class MyTask extends AsyncTask<Integer, Float, String>{
+
+        //Thread에 해당
+        @Override
+        protected String doInBackground(Integer... params) {
+            int max = params[0];
+            //int a = params[1];
+            //int b = params[2];
+            for (int i=0 ; i < max ; i++){
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Log.d("async", "count:"+i);
+            }
+            return null;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MyTask mytask = new MyTask();
+        mytask.execute(100, 200, 300);
     }
 }
