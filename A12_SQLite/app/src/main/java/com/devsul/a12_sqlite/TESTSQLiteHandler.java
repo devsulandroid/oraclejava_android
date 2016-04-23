d70598ea1a3dd54b06aa04d653d692eb1214dbf4
설의석 <devsul@naver.com>
2016-04-23 15:28:21 +0900

    SQLite

--- A12_SQLite/app/src/main/java/com/devsul/a12_sqlite/TESTSQLiteHandler.java
+++ A12_SQLite/app/src/main/java/com/devsul/a12_sqlite/TESTSQLiteHandler.java
@@ -2,6 +2,7 @@ package com.devsul.a12_sqlite;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 /**
@@ -26,5 +27,43 @@ public class TESTSQLiteHandler {
         values.put("address", address);
 
         db.insert("USER", null, values);
+    }
+
+    //delete
+    public void delete(String name){
+        db = helper.getWritableDatabase();
+
+        db.delete("USER", "name = ?", new String[] {name});
+    }
+
+    //update
+    public void update(String name, int newAge){
+        db = helper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put("age", newAge);
+
+        db.update("USER", values, "name = ?", new String[]{name});
+
+    }
+
+    //select
+    public String selectAll(){
+        db = helper.getWritableDatabase();
+
+        String res = "";
+        Cursor c = db.query("USER", null, null, null, null, null, null);
+
+        while (c.moveToNext()){
+            int id = c.getInt(c.getColumnIndex("id"));
+            String name = c.getString(c.getColumnIndex("name"));
+            int age = c.getInt(c.getColumnIndex("age"));
+            String address = c.getString(c.getColumnIndex("address"));
+
+            res += "id : " + id + ", name : " + name + ", age : " + age + ", address : " + address;
+            res += "\n";
+        }
+
+        return res;
     }
 }
