3c01123abc9ae2aabcbecaeed95036aa94d324dc
설의석 <devsul@naver.com>
2016-04-23 12:08:22 +0900

    Thread 2

---
+++ A10_AsycTask/app/src/main/java/com/devsul/a10_asyctask/MainActivity.java
@@ -0,0 +1,13 @@
+package com.devsul.a10_asyctask;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
