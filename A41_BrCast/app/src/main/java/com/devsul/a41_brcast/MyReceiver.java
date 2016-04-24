b3ed77cd8d4aca1c7f73b95535444d2ca1c00118
설의석 <devsul@naver.com>
2016-04-24 11:27:56 +0900

    브로드캐스트

--- A41_BrCast/app/src/main/java/com/devsul/a41_brcast/MyReceiver.java
+++ A41_BrCast/app/src/main/java/com/devsul/a41_brcast/MyReceiver.java
@@ -3,15 +3,34 @@ package com.devsul.a41_brcast;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
 
 public class MyReceiver extends BroadcastReceiver {
     public MyReceiver() {
     }
 
+    // 문자 정보가 intent 통해 넘어온다
     @Override
     public void onReceive(Context context, Intent intent) {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
-        throw new UnsupportedOperationException("Not yet implemented");
+        //throw new UnsupportedOperationException("Not yet implemented");
+
+        SmsMessage[] smsMessages = null;
+        Object[] objects = (Object[]) intent.getExtras().get("pdus");
+        smsMessages = new SmsMessage[objects.length];
+
+        String str = "";
+        for(int i=0 ; i < smsMessages.length ; i++){
+            smsMessages[i] = SmsMessage.createFromPdu((byte[])objects[i]) ;
+
+            str += "SMS from : " + smsMessages[i].getOriginatingAddress();
+            str += " :  + " + smsMessages[i].getMessageBody() + "\n";
+
+        }
+
+        Toast.makeText(context, str, Toast.LENGTH_LONG).show();
+
     }
 }
