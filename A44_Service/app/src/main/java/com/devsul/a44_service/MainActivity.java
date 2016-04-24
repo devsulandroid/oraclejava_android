329bfa9d5fbce221835b9f9e94ce74f86977f3fc
설의석 <devsul@naver.com>
2016-04-24 14:33:24 +0900

    service 심화

--- A44_Service/app/src/main/java/com/devsul/a44_service/MainActivity.java
+++ A44_Service/app/src/main/java/com/devsul/a44_service/MainActivity.java
@@ -1,13 +1,48 @@
 package com.devsul.a44_service;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(this, MyService.class);
+        bindService(intent, serviceConnection, BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unbindService(serviceConnection);
+    }
+
+    public void btnClick(View v){
+
     }
 }
---
+++ A44_Service/app/src/main/java/com/devsul/a44_service/MyService.java
@@ -0,0 +1,54 @@
+package com.devsul.a44_service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+
+import java.util.Random;
+
+public class MyService extends Service {
+    MyBinder myBinder = new MyBinder();
+
+    public MyService() {
+
+    }
+
+    public class MyBinder extends Binder{
+        public MyService getService() {
+            return MyService.this;
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        //throw new UnsupportedOperationException("Not yet implemented");
+        return myBinder;
+    }
+
+    private Random random = new Random();
+
+    public int getRandom(){
+        return random.nextInt(100);
+    }
+
+
+
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
