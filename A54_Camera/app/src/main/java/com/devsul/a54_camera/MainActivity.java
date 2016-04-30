9a273f325b52f34677aafb26ab95652cfa61defb
설의석 <devsul@naver.com>
2016-04-30 17:21:10 +0900

    Camera

--- A54_Camera/app/src/main/java/com/devsul/a54_camera/MainActivity.java
+++ A54_Camera/app/src/main/java/com/devsul/a54_camera/MainActivity.java
@@ -1,8 +1,14 @@
 package com.devsul.a54_camera;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +18,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    int TAKE_PHOTO = 1;
     public void onBtnClick(View v){
+        String dirPath = Environment.getExternalStorageDirectory()+"/devsul";
+        File dir = new File(dirPath);
 
+        if(!dir.exists()){
+            // 하위 디렉토리까지 생성
+            dir.mkdirs();
+        }
+
+        // 내부 저장소
+        String path = dirPath + "/26.jpg";
+
+        //암시적 인텐드
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // 내부 리소스 주소 소문자 Uri
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(new File(path)));
+
+        startActivityForResult(intent, TAKE_PHOTO);
     }
 }
