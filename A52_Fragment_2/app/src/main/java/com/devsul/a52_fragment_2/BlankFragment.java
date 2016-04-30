780c3c750802ad63427e2f8a9318532876f0fd5c
설의석 <devsul@naver.com>
2016-04-30 13:52:11 +0900

    fragment 2

---
+++ A52_Fragment_2/app/src/main/java/com/devsul/a52_fragment_2/BlankFragment.java
@@ -0,0 +1,36 @@
+package com.devsul.a52_fragment_2;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BlankFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_blank, container, false);
+        final TextView textViewCounter = (TextView) view.findViewById(R.id.textViewCounter);
+        Button btnIncrease = (Button)view.findViewById(R.id.btnIncrease);
+        btnIncrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String str = textViewCounter.getText().toString();
+                int value = Integer.parseInt(str);
+                value++;
+                textViewCounter.setText(""+value);
+            }
+        });
+        return view;
+    }
+
+}
