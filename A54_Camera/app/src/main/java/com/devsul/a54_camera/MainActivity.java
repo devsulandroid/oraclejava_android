aab16c8b8e96880a897cbedd8f302245cff97bd3
설의석 <devsul@naver.com>
2016-04-30 18:16:44 +0900

    Camera

--- A54_Camera/app/src/main/java/com/devsul/a54_camera/MainActivity.java
+++ A54_Camera/app/src/main/java/com/devsul/a54_camera/MainActivity.java
@@ -1,12 +1,15 @@
 package com.devsul.a54_camera;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 
 import java.io.File;
 
@@ -38,5 +41,27 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(new File(path)));
 
         startActivityForResult(intent, TAKE_PHOTO);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == TAKE_PHOTO){
+            if(resultCode == RESULT_OK){
+                String dirPath = Environment.getExternalStorageDirectory()+"/devsul";
+                String path = dirPath + "/26.jpg";
+
+                ImageView imageView = (ImageView) findViewById(R.id.btnImgView);
+
+                //파일로 있을때
+                Bitmap bitmap;
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                //4px 당 1로 로딩
+                options.inSampleSize = 4;
+
+                bitmap = BitmapFactory.decodeFile(path, options);
+                imageView.setImageBitmap(bitmap);
+            }
+        }
     }
 }
