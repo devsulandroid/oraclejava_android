6ce6a7473baaa7f2a650db29324c860c20c4afa9
설의석 <devsul@naver.com>
2016-04-30 14:41:47 +0900

    fragment 2

--- A52_Fragment_2/app/src/main/java/com/devsul/a52_fragment_2/MainActivity.java
+++ A52_Fragment_2/app/src/main/java/com/devsul/a52_fragment_2/MainActivity.java
@@ -1,7 +1,11 @@
 package com.devsul.a52_fragment_2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onBtnClick(View v){
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fr = null;
+
+        switch (v.getId()){
+            case R.id.btnAdd:
+                if(fr == null){
+                    fr = new BlankFragment();
+                    FragmentTransaction ft = fm.beginTransaction();
+                    ft.add(R.id.frame, fr, "counter");
+                    ft.commit();
+                }
+                break;
+            case R.id.btnRemove:
+                break;
+            case R.id.btnReplace:
+                break;
+            case R.id.btnHide:
+                break;
+        }
     }
 }
