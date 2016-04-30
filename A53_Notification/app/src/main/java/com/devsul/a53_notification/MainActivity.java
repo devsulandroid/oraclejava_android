93af3e50f423052bc3ad23fda7ff316a2cb7332b
설의석 <devsul@naver.com>
2016-04-30 16:09:06 +0900

    notification

--- A53_Notification/app/src/main/java/com/devsul/a53_notification/MainActivity.java
+++ A53_Notification/app/src/main/java/com/devsul/a53_notification/MainActivity.java
@@ -2,6 +2,8 @@ package com.devsul.a53_notification;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.NotificationCompat;
@@ -18,12 +20,15 @@ public class MainActivity extends AppCompatActivity {
     public void onBtnClick(View v){
         NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
+        //리소스 res 에서 이미지 가죠오기
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.koala);
+
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
         builder.setContentTitle("TITLE");
         builder.setContentText("TEXT");
         builder.setSubText("SubTEXT");
         builder.setSmallIcon(R.mipmap.ic_launcher);
-
+        builder.setLargeIcon(bitmap);
         Notification notification = builder.build();
         notificationManager.notify(1234, notification);
     }
