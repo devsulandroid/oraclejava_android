de31f25642c4fad51eee4013db8fd0be70eebf48
설의석 <devsul@naver.com>
2016-04-30 11:36:58 +0900

    액션 윈도구

--- A51_FloatWindow/app/src/main/java/com/devsul/a51_floatwindow/FloatingService.java
+++ A51_FloatWindow/app/src/main/java/com/devsul/a51_floatwindow/FloatingService.java
@@ -24,84 +24,74 @@ public class FloatingService extends Service {
     }
 
     WindowManager wm;
-    WindowManager.LayoutParams params;
     LinearLayout ll;
+    Button btnStop;
+    WindowManager.LayoutParams params;
 
 
     @Override
     public void onCreate() {
         super.onCreate();
-
         wm = (WindowManager) getSystemService(WINDOW_SERVICE);
         ll = new LinearLayout(this);
-        Button btnStop;
-
         LinearLayout.LayoutParams llParams = new LinearLayout.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.MATCH_PARENT
-
         );
-
-        ll.setBackgroundColor(Color.argb(55,255,0,0));
+        ll.setBackgroundColor(Color.argb(55, 255, 0,0));
         ll.setLayoutParams(llParams);
-        ll.setOnTouchListener(new View.OnTouchListener() {
-            WindowManager.LayoutParams updateParams = params;
 
-            int x,y;
-            float touchedX, touchedY;
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                switch (event.getAction()){
-                    case MotionEvent.ACTION_DOWN:
-                        x = updateParams.x;
-                        y = updateParams.y;
-
-                        touchedX = event.getRawX();
-                        touchedY = event.getRawY();
-                        break;
-                    case MotionEvent.ACTION_MOVE:
-                        updateParams.x = x + (int)(event.getRawX() - touchedX);
-                        updateParams.y = y + (int)(event.getRawY() - touchedY);
-                        wm.updateViewLayout(ll, updateParams);
-                        break;
-                }
-                return false;
-            }
-        });
 
         btnStop = new Button(this);
         ViewGroup.LayoutParams btnParams = new ViewGroup.LayoutParams(
                 ViewGroup.LayoutParams.WRAP_CONTENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT
-
         );
-
         btnStop.setLayoutParams(btnParams);
         btnStop.setText("stop");
-
         ll.addView(btnStop);
         btnStop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //화면 종료
                 wm.removeView(ll);
-                //서비스 종료
                 stopSelf();
             }
         });
 
-       params = new WindowManager.LayoutParams(
-                400,160,
+        params = new WindowManager.LayoutParams(
+                400, 160,
                 WindowManager.LayoutParams.TYPE_PHONE,
                 WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-                PixelFormat.TRANSLUCENT
+                PixelFormat.TRANSPARENT
         );
-
         params.x = 0;
         params.y = 0;
-
         params.gravity = Gravity.CENTER | Gravity.CENTER;
 
         wm.addView(ll, params);
+
+        ll.setOnTouchListener(new View.OnTouchListener() {
+            WindowManager.LayoutParams updatedParams = params;
+            int x,y;
+            float touchedX, touchedY;
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+                        x = updatedParams.x;
+                        y = updatedParams.y;
+
+                        touchedX = motionEvent.getRawX();
+                        touchedY = motionEvent.getRawY();
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        updatedParams.x = x + (int)(motionEvent.getRawX() - touchedX);
+                        updatedParams.y = y + (int)(motionEvent.getRawY() - touchedY);
+                        wm.updateViewLayout(ll, updatedParams);
+                        break;
+                }
+                return false;
+            }
+        });
     }
 }
