f1bc8a9f95f1ec0c9e285f61dbd1f3b1eef1c956
설의석 <devsul@naver.com>
2016-04-30 15:50:41 +0900

    notification

--- A53_Notification/app/src/main/java/com/devsul/a53_notification/NotiActivity.java
+++ A53_Notification/app/src/main/java/com/devsul/a53_notification/NotiActivity.java
@@ -2,17 +2,12 @@ package com.devsul.a53_notification;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class NotiActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void onBtnClick(View v){
-
+        setContentView(R.layout.activity_noti);
     }
 }
